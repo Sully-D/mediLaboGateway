4f2a28fb7eaea9db7b20ae15a9e1e7614b764ef3
sully <delemos.sullivan@gmail.com>
2024-08-26 12:00:13 +0200

    Add logs

--- src/main/java/com/medilabo/gateway/config/SecurityConfig.java
+++ src/main/java/com/medilabo/gateway/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.medilabo.gateway.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -20,27 +22,38 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        logger.debug("Creating BCryptPasswordEncoder bean");
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        logger.debug("Configuring in-memory user details service");
+
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("user")
                 .password(bCryptPasswordEncoder.encode("user"))
                 .roles("USER")
                 .build());
+        logger.info("In-memory user created: user with role USER");
+
         manager.createUser(User.withUsername("admin")
                 .password(bCryptPasswordEncoder.encode("admin"))
                 .roles("USER", "ADMIN")
                 .build());
+        logger.info("In-memory user created: admin with roles USER, ADMIN");
+
         return manager;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        logger.debug("Configuring security filter chain");
+
         http.csrf().disable()
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
@@ -52,11 +65,15 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        logger.info("Security filter chain configured");
+
         return http.build();
     }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
+        logger.debug("Configuring web security customizer to ignore static resources");
+
         return web -> web.debug(true).ignoring().requestMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico");
     }
 }
